944f7814383ef128fa63a736ea7acb94406ac51c
Felipe Mendes <felipemendesara123@gmail.com>
2016-04-06 17:37:02 -0300

    Sistema de Indentificação de Usuário commitado!

---
+++ TestIndentity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestIndentity.Startup))]
+namespace TestIndentity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
